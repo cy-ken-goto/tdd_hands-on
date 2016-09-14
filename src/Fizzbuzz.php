d4daf6cbae338a34c6a6eaaa0e31aad140475a28
cy-ken-goto <ken_goto@cybird.co.jp>
2016-09-14 19:02:25 +0900

    3でfizz,5でbuzzを返すようにした

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -3,6 +3,12 @@ namespace App;
 class Fizzbuzz
 {
   public function calc($num){
+    if ($num % 3 === 0) {
+      return 'Fizz';
+    }
+    if ($num % 5 === 0) {
+      return 'Buzz';
+    }
     return $num;
   }
 }
--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -11,4 +11,24 @@ class FizzbuzzTest extends \PHPUnit_Framework_TestCase
     $expected = 1;
     $this->assertSame($expected, $fizzbuzz->calc(1));
   }
+
+  /**
+   * @test
+   */
+  public function ３の倍数のときにFizzを返すこと()
+  {
+    $fizzbuzz = new Fizzbuzz();
+    $expected = 'Fizz';
+    $this->assertSame($expected, $fizzbuzz->calc(3));
+  }
+
+  /**
+   * @test
+   */
+  public function ５の倍数のときにBuzzを返すこと()
+  {
+    $fizzbuzz = new Fizzbuzz();
+    $expected = 'Buzz';
+    $this->assertSame($expected, $fizzbuzz->calc(5));
+  }
 }
